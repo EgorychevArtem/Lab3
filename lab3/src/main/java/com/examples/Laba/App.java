9bec544cbed3db67b149a2923c8ae65b3c47d5ab
Egorychev Artem <temych_2000@mail.ru>
2019-10-23 12:58:18 +0300

    Scripted auto-commit on change (2019-10-23 12:58:18) by gitwatch.sh

--- lab3/src/main/java/com/examples/Laba/App.java
+++ lab3/src/main/java/com/examples/Laba/App.java
@@ -1,5 +1,7 @@
 package com.examples.Laba;
 
+import org.apache.spark.Sparkconf;
 public class App {
-
+    SparkConf conf = new SparkConf().setAppName("lab5");
+    JavaSparkContext sc = new JavaSparkContext(conf);
 }
